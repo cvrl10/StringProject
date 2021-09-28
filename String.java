92cc17060d7b0eab8d0803d1918a311eea231fb5
cvrl10 <91512002+cvrl10@users.noreply.github.com>
2021-09-28 18:42:02 -0400

    Create String.java
    two-dimensional array

---
+++ String.java
@@ -0,0 +1,17 @@
+public class String
+{
+  public static void main(String[] args)
+  {
+    String[][] name = {{"Carl","Archemetre"},{"Rose","Archemetre"}};
+    
+    for (String str : name)
+      System.out.println(str);
+  
+  
+  
+  
+  
+  
+  
+ }
+}
